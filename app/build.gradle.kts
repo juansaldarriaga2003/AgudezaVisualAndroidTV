52d4a7d3330aed7c57290e78d3a5d0e92e53388b
juansaldarriaga2003 <juansaldarriaga2003@gmail.com>
2026-07-31 18:27:03 -0500

    Publicar corrección 2.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.agudezavisual.tv"
         minSdk = 21
         targetSdk = 35
-        versionCode = 6
-        versionName = "2.2.0"
+        versionCode = 7
+        versionName = "2.2.1"
     }
 
     flavorDimensions += "deployment"
