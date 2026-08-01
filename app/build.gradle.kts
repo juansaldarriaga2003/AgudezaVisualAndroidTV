9b1dc38b3ca91ad4ab3c0b62d018a025637d3cf9
juansaldarriaga2003 <juansaldarriaga2003@gmail.com>
2026-08-01 01:18:49 -0500

    Publicar revisión kiosco 2.3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.agudezavisual.tv"
         minSdk = 21
         targetSdk = 35
-        versionCode = 10
-        versionName = "2.3.2"
+        versionCode = 11
+        versionName = "2.3.3"
     }
 
     flavorDimensions += "deployment"
