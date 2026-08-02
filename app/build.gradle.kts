54bcf6de48a0913ecb62e09a579c0f1c65c57924
juansaldarriaga2003 <juansaldarriaga2003@gmail.com>
2026-08-02 08:37:42 -0500

    chore: publicar versión 3.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.agudezavisual.tv"
         minSdk = 21
         targetSdk = 35
-        versionCode = 13
-        versionName = "3.0.0"
+        versionCode = 14
+        versionName = "3.1.0"
     }
 
     flavorDimensions += "deployment"
